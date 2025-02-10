0ef4b97821a97efb78591c766e3450ebb0cccb11
umfb <umfbankltd1@gmail.com>
2025-02-10 08:32:00 +0100

    depts dismantled

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import {
   AdminPanelSettings,
-  Business,
+  // Business,
   ChevronLeftRounded,
   ChevronRightRounded,
   History,
-  KeyboardArrowDown,
-  KeyboardArrowUp,
   PeopleOutlineRounded,
 } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
 
 export default function SideBar() {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
-  const [isListVisible, setIsListVisible] = useState<boolean>(false);
+  // const [isListVisible, setIsListVisible] = useState<boolean>(false);
 
-  const toggleListVisibility = () => {
-    setIsListVisible(!isListVisible);
-  };
+  // const toggleListVisibility = () => {
+  //   setIsListVisible(!isListVisible);
+  // };
 
   return (
     <aside
@@ -41,128 +39,107 @@ export default function SideBar() {
             {isExpanded ? <ChevronLeftRounded /> : <ChevronRightRounded />}
           </button>
         </div>
-        <ul className="flex flex-col gap-3 mt-3 px-0 text-[#1C1C1C]">
-          <div
-            className="flex items-center gap-3"
-            style={{ textDecoration: "none" }}
-          >
-            <div className="flex items-center gap-3 ps-3">
-              <button className="text-[#1C1C1C]">
-                <Business />
-              </button>
-              {isExpanded && <button>Departments</button>}
-            </div>
-            <button
-              onClick={(event) => {
-                event.stopPropagation();
-                toggleListVisibility();
-              }}
-            >
-              {isListVisible ? (
-                <KeyboardArrowUp
-                  style={{
-                    height: "20px",
-                    color: "gray",
-                  }}
-                />
-              ) : (
-                <KeyboardArrowDown
-                  style={{
-                    height: "20px",
-                    color: "gray",
-                  }}
-                />
-              )}
-            </button>
-          </div>
-          <div
-            className="m-0 p-0"
+        <ul className="flex flex-col mt-3 px-0 text-[#1C1C1C]">
+          <NavLink
+            to="./hr"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
             style={{
-              overflow: "hidden",
-              transition: "max-height 0.5s ease-in-out",
-              maxHeight: isListVisible ? "200px" : "0",
-              padding: "0",
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
             }}
           >
-            <div className="flex flex-col ms-4">
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                HR
-              </NavLink>
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                Finance & Ops
-              </NavLink>
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1 active:bg-red-500"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                Credit & Marketing
-              </NavLink>
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                Audit
-              </NavLink>
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                Compliance
-              </NavLink>
-              <NavLink
-                to=""
-                className="text-gray-700 hover:bg-[#dcee96] hover:text-black ps-3 py-1"
-                style={{
-                  textDecoration: "none",
-                  color: "#7b3434",
-                  fontSize: "14px",
-                  fontWeight: "500",
-                }}
-              >
-                IT
-              </NavLink>
-            </div>
-          </div>
+            HR
+          </NavLink>
+          <NavLink
+            to="./finOps"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
+            style={{
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+          >
+            Finance & Ops
+          </NavLink>
+          <NavLink
+            to="crM"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
+            style={{
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+          >
+            Credit & Marketing
+          </NavLink>
+          <NavLink
+            to="audit"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
+            style={{
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+          >
+            Audit
+          </NavLink>
+          <NavLink
+            to="compliance"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
+            style={{
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+          >
+            Compliance
+          </NavLink>
+          <NavLink
+            to="./info-tech"
+            className={({ isActive }) =>
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
+                isActive ? "bg-[#BB7A5A]" : ""
+              }`
+            }
+            style={{
+              textDecoration: "none",
+              color: "#7b3434",
+              fontSize: "14px",
+              fontWeight: "500",
+            }}
+          >
+            IT
+          </NavLink>
           <NavLink
             to="staff"
             className={({ isActive }) =>
-              `flex items-center gap-3 cursor-pointer ps-3 py-3 hover:bg-[#C8E363] ${
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
                 isActive ? "bg-[#BB7A5A]" : ""
               }`
             }
@@ -179,7 +156,7 @@ export default function SideBar() {
           <NavLink
             to="./roles"
             className={({ isActive }) =>
-              `flex items-center gap-3 cursor-pointer ps-3 py-3 hover:bg-[#C8E363] ${
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
                 isActive ? "bg-[#BB7A5A]" : ""
               }`
             }
@@ -196,7 +173,7 @@ export default function SideBar() {
           <NavLink
             to="./activities"
             className={({ isActive }) =>
-              `flex items-center gap-3 cursor-pointer ps-3 py-3 hover:bg-[#C8E363] ${
+              `flex items-center gap-3 cursor-pointer ps-3 py-2 hover:bg-[#C8E363] ${
                 isActive ? "bg-[#BB7A5A]" : ""
               }`
             }
